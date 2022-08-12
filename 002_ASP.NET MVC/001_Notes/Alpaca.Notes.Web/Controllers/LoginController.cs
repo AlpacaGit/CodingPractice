6a157a3ad1a8c00944c7c71ae1241016ed5f0820
Alpaca <sushingoro@gmail.com>
2022-08-12 21:20:45 +0900

    ログイン処理実装のためのコントローラ追加。画面名の名称を定数用クラスから取得するように変更してみる。

---
+++ 002_ASP.NET MVC/001_Notes/Alpaca.Notes.Web/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alpaca.Notes.Web.Common;
+
+namespace Alpaca.Notes.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            //未実装
+            if(System.Web.HttpContext.Current.Session[SessionVariable.LOGIN_USERID] == null || System.Web.HttpContext.Current.Session[SessionVariable.LOGIN_USERID].ToString() == "")
+            {
+                System.Web.HttpContext.Current.Session[SessionVariable.LOGIN_USERID] = "Alpaca";
+            }
+            else
+            {
+                System.Web.HttpContext.Current.Session.Remove(SessionVariable.LOGIN_USERID);
+            }
+            return View();
+        }
+    }
+}
